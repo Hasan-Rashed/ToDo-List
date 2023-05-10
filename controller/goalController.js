221d76ddf17de57700fd0998897063a1ac0bc52c
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-10 10:06:56 +0600

    route and controller without database

---
+++ controller/goalController.js
@@ -0,0 +1,27 @@
+const getGoals = (req, res) => {
+    res.status(200)
+    .json({ success: true, data: [{ "id": 1, "name": "Goal 1" }, { "id": 2, "name": "Goal 2" }] })
+}
+
+const setGoals = (req, res) => {
+    res.status(200)
+    .json({ success: true, message: "Set goal" })
+}
+
+const updateGoal = (req, res) => {
+    res.status(200)
+    .json({ success: true, message: `Update goal ${req.params.id}` })
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200)
+    .json({ success: true, message: `Delete goal ${req.params.id}` })
+}
+
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal
+}
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 
-const port = 5000;
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.listen(port, () => `Server running on port ${port} 🔥`);
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

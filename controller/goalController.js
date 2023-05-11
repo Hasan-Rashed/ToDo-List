4c241a414784a2dfcb508bb5bb717efe1644f3e3
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-11 09:55:18 +0600

    express-async-handler

--- controller/goalController.js
+++ controller/goalController.js
@@ -1,9 +1,14 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler'); 
+
+
+// using express-async-handler to handle errors instead of try catch blocks
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200)
     .json({ success: true, data: [{ "id": 1, "name": "Goal 1" }, { "id": 2, "name": "Goal 2" }] })
-}
+})
 
-const setGoals = (req, res) => {
+// using express-async-handler to handle errors instead of try catch blocks
+const setGoals = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field')
@@ -11,17 +16,19 @@ const setGoals = (req, res) => {
     
     res.status(200)
     .json({ success: true, message: "Set goal" })
-}
+})
 
-const updateGoal = (req, res) => {
+// using express-async-handler to handle errors instead of try catch blocks
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200)
     .json({ success: true, message: `Update goal ${req.params.id}` })
-}
+})
 
-const deleteGoal = (req, res) => {
+// using express-async-handler to handle errors instead of try catch blocks
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200)
     .json({ success: true, message: `Delete goal ${req.params.id}` })
-}
+})
 
 
 module.exports = {

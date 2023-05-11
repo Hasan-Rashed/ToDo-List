34b881dfdeb0084cd843cc04ee7cc8f39d2d36a2
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-11 08:53:57 +0600

    accepting body data

--- controller/goalController.js
+++ controller/goalController.js
@@ -4,6 +4,7 @@ const getGoals = (req, res) => {
 }
 
 const setGoals = (req, res) => {
+    console.log(req.body);
     res.status(200)
     .json({ success: true, message: "Set goal" })
 }
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
+// Middleware
+// difference between the two is the type of data they can handle. express.json() is used for parsing JSON data, while express.urlencoded() is used for parsing URL-encoded form data.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

d27d4628110e3b40e874eab23903bfd5d9e88ab5
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-17 05:46:15 +0600

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 
 dotenv.config();
@@ -10,6 +11,9 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+// CORS
+app.use(cors());
 
 
 // Middleware

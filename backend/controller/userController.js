a441c1007fb8aa3ec794c3262e63e476b51d8de8
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-31 11:51:36 +0600

    register User function

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -23,10 +23,28 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400) // 400 = bad request
         throw new Error('User already exists')
     }
-    
-    res.json({
-        message: 'Your account has been registered!'
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create a new user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id, // brad has uses user.id
+            name: user.name,
+            email: user.email
+    })
+}else {
+    res.status(400)
+    throw new Error('Invalid user data')
+}
 })
 
 

07820ba96d21d755692d2cdeed957ece5c4af0b5
Hasan Rashed <hasanrashed537@gmail.com>
2023-05-31 10:46:51 +0600

    add user to goal model

--- backend/model/goalModel.js
+++ backend/model/goalModel.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 
 
 const goalSchema = mongoose.Schema({
+    // user object will be used to identify the user who created the goal
+    // it will allow us to have a user associated with a goal
+    user: {
+/* `user` field will store a reference to another document in the database, 
+specifically a document in the `User` collection. The `ref: 'User' property 
+specifies the name of the referenced collection. */
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     text: {
         type: String,
         required: true
